9845f6eee9926910460e47abb75b07f7a6ff6d35
Breno <bcaua205@gmail.com>
2022-06-04 03:20:27 -0300

    Finalizando front-end

--- wwwroot/js/validation.js
+++ wwwroot/js/validation.js
@@ -1,72 +1,86 @@
 ﻿const form = document.getElementById("form");
-const elementNome = form.elements['fornecedor'];
-const elementCnpj = form.elements['cnpj'];
+const elementCnpj = document.getElementById("cnpj");
 
-const nome = elementNome.value;
-const cnpj = elementCnpj.value;
-
-form.addEventListener("submit", formValidation);
-
-function formValidation(e){
+form.addEventListener("submit", (e) => {
   e.preventDefault();
+  const elementNome = form.elements[0];
+  const elementCnpj = form.elements[1];
+
+  const nome = elementNome.value;
+  const cnpj = elementCnpj.value;
+
+
   let nameInputIsValid = nameIsValid(nome, elementNome);
   let cnpjInputIsValid = cnpjIsValid(cnpj, elementCnpj);
 
   if(nameInputIsValid && cnpjInputIsValid){
-    form.Submit();
+    form.submit();
   }
-}
+});
 
 function cnpjIsValid(str, node){  
   if(!hasValue(str)){
-    addMsg("Campo vazio", node, "invalid");
+    addMsg("Campo vazio", node, false);
     return false;
   }
 
   if(str.length != 14){
-    addMsg("Campo incorreto", node, "invalid");
+    addMsg("Campo incorreto", node, false);
     return false;
   }
   
-  str.toInt();
+  parseInt(str);
 
   if(isNaN(str)){
-    addMsg("Campo deve ser númerico.", node, "invalid");
+    addMsg("Campo deve ser númerico.", node, false);
     
     return false;
   }
 
   
-  addMsg("", node, "valid");
+  addMsg("", node, true);
   return true;
 }
 
 function nameIsValid(str, node){
   if(!hasValue(str)){
-    addMsg("Campo vazio", node, "invalid");
+    addMsg("Campo vazio", node, false);
     return false;
   }
   
   if(str.length > 100){
-    addMsg("Nome com mais de 100 caracteres", node, "invalid");
+    console.log("Tamanho " + str.length);
+    addMsg("Nome com mais de 100 caracteres", node, false);
     return false;
   }
 
-  addMsg("", node, "valid");
+  addMsg("", node, true);
   return true;
 }
 
 // Para adicionar a mensagem e formatação
 function addMsg(str, node, isValid){
-  const message = node.parentNode.getElementsByName("small");
+  const message = node.parentNode.querySelector("small");
+  if(isValid){
+    node.classList.remove("invalid-input"); 
+    message.innerText = str;
+    message.className = "text-sucess";
+    
+    node.classList.add("valid-input"); 
+    return;
+  }
+  
+  node.classList.remove("valid-input"); 
   message.innerText = str;
-  message.className = isValid;
-  node.className = isValid; 
+  message.className = "text-danger";
+  
+  node.classList.add("invalid-input"); 
 }
 
 // Verificar se tem valor
 function hasValue(input){
-  if(input.trim().length === 0){
+  if(input.trim().length == 0){
+    console.log("o valor é: " + input);
     return false;
   }
 
